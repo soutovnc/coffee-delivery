666b293b42c36b2762a7b3fe4024abed566ac8aa
Vinicius <viniciusdesouto@gmail.com>
2023-09-17 21:39:10 -0300

    add fonts roboto, baloo 2 and default styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    "yellow-dark": "#C47f17",
+    "yellow": "#DBAC2C",
+    "yellow-light": "#F1E9C9",
+    "purple-dark": "#4B2995",
+    "purple": "#8047F8",
+    "purple-light": "#EBE5F9",
+
+    "base-title": "#272221",
+    "base-subtitle": "#403937",
+    "base-text": "#574F4D",
+    "base-label": "#8D8686",
+    "base-hover": "#D7D5D5",
+    "base-button": "#E6E5E5",
+    "base-input": "#EDEDED",
+    "base-card": "#F3F2F2",
+
+    "background": "#FAFAFA",
+    "white": "#FFFFFF",
+  },
+  textSizes: {
+    "title-xl": "3rem",
+    "title-l": "2",
+    "title-m": "1.5rem",
+    "title-s": "1.25rem",
+    "title-xs": "1.125rem",
+
+    "text-l": "1.25rem",
+    "text-m": "1rem",
+    "text-s": ".875rem",
+    "text-xs": ".75rem",
+
+    "tag": ".625rem",
+    "button-g": ".875rem",
+    "button-m": ".75rem"
+  },
+  fonts: {
+    "base": "Roboto",
+    "title": "Baloo 2"
+  }
+}
