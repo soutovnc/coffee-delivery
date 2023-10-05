4e8d19e320a3e00874688158215ac53b387478e1
Vinicius <viniciusdesouto@gmail.com>
2023-10-04 23:57:28 -0300

    add input at address form

--- src/pages/Checkout/components/CompleteOrder/AddressForm.tsx
+++ src/pages/Checkout/components/CompleteOrder/AddressForm.tsx
@@ -1,5 +1,6 @@
 import { useFormContext } from "react-hook-form";
 import { AddressFormContainer } from "./styles";
+import { Input } from "../../../../components/Input";
 
 interface ErrorsType {
   errors: {
@@ -16,7 +17,47 @@ export function AddressForm() {
 
   return (
     <AddressFormContainer>
-
+      <Input
+        placeholder="CEP"
+        type="number"
+        className="cep"
+        {...register("cep")}
+        error={errors.cep?.message}
+      />
+      <Input
+        placeholder="Rua"
+        className="street"
+        {...register("street")}
+        error={errors.street?.message}
+      />
+      <Input
+        placeholder="Número"
+        type="number"
+        {...register("number")}
+        error={errors.number?.message}
+      />
+      <Input
+        placeholder="Complemento"
+        className="complement"
+        {...register("complement")}
+        error={errors.complement?.message}
+        rightText="Opcional"
+      />
+      <Input
+        placeholder="Bairro"
+        {...register("district")}
+        error={errors.district?.message}
+      />
+      <Input
+        placeholder="Cidade"
+        {...register("city")}
+        error={errors.city?.message}
+      />
+      <Input
+        placeholder="UF"
+        {...register("uf")}
+        error={errors.uf?.message}
+      />
     </AddressFormContainer>
   )
 
